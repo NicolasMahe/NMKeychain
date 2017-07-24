2dd72bc4bd8160d749dd4199b39ecc6a6b818d0c
Nicolas Mahé <nicolas@mahe.me>
2017-07-24 16:58:49 +0700

    add example project with test

--- Example/NMKeychain/ViewController.swift
+++ Example/NMKeychain/ViewController.swift
@@ -7,18 +7,49 @@
 //
 
 import UIKit
+import NMKeychain
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+  
+  @IBOutlet weak var label: UILabel!
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+    
+    self.example()
+  }
+  
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+  
+  //----------------------------------------------------------------------------
+  // MARK: - Example
+  //----------------------------------------------------------------------------
+  
+  private let dataInKeychain = NMKeychain<Date?>(
+    key: "dataInKeychain_unique_key",
+    identifier: "unique_keychain_identifier",
+    label: "Label for this data",
+    defaultValue: Date(timeIntervalSinceNow: -600),
+    synchronizable: true,
+    accessibility: .afterFirstUnlock,
+    onChange: {
+      print("data has changed")
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
+  )
+  
+  func example() {
+    print("should return the default value")
+    print(self.dataInKeychain.value as Any)
+    
+    // set a value
+    self.dataInKeychain.value = Date()
+    print("should return the new value")
+    print(self.dataInKeychain.value as Any)
+  }
+  
 }
 
